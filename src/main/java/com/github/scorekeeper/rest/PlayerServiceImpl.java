22dcb2ca5402104f1aaf2b6888d5011509a1b3b5
Andre Marbeck <andre.marbeck@viaprinto.de>
2013-06-05 07:34:08 +0200

    add testservice

---
+++ src/main/java/com/github/scorekeeper/rest/PlayerServiceImpl.java
@@ -0,0 +1,35 @@
+package com.github.scorekeeper.rest;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.github.scorekeeper.persistence.dao.PlayerRepository;
+import com.github.scorekeeper.persistence.entity.Player;
+
+/*
+Creation date: 04.06.2013
+Created by andre.marbeck
+Project: scorekeeper
+
+Copyright diron 2013
+*/
+
+@Service
+public class PlayerServiceImpl implements PlayerService {
+
+	@Resource
+	public PlayerRepository repo;
+
+	/* (non-Javadoc)
+	 * @see com.github.scorekeeper.rest.PlayerService#findById(java.lang.Long)
+	 */
+	@Override
+	@Transactional
+	public Player findById(Long id) {
+		return repo.findOne(id);
+
+	}
+
+}
---
+++ src/main/java/com/github/scorekeeper/rest/ScorekeeperService.java
@@ -0,0 +1,44 @@
+/*
+Creation date: 04.06.2013
+Created by andre.marbeck
+Project: scorekeeper
+
+Copyright diron 2013
+*/
+
+package com.github.scorekeeper.rest;
+
+import javax.annotation.Resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.github.scorekeeper.persistence.dao.GameRepository;
+import com.github.scorekeeper.persistence.dao.PlayerRepository;
+import com.github.scorekeeper.persistence.entity.Player;
+
+@Controller
+public class ScorekeeperService {
+
+	@Resource
+	public PlayerRepository playerRepository;
+
+	@Resource
+	public GameRepository gameRepository;
+
+	@Resource
+	private PlayerService service;
+
+	@RequestMapping(value = "players", method = RequestMethod.GET, produces = "application/json")
+	@ResponseStatus(value = HttpStatus.OK)
+	@ResponseBody
+	public Player getAllPlayers() {
+
+		return service.findById(new Long(14));
+
+	}
+}
